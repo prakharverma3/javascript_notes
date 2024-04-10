d7c58570dd188f3269289de6ba31171e5fd1628a
Prakhar Verma <prakharverma3march@gmail.com>
2024-04-10 17:43:52 +0530

    starter

---
+++ 01_basic/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")
+// alert(3+3) // cant use with this syntax
+
+let name="Prakahr"
+
+/* string 
+* number
+* boolean (true/false)
+null
+undefined
+symbol-> used for uniqueness
+
+
+object
+
+// */
+console.log(typeof undefined);
+console.log(typeof null);
+
---
+++ 01_basic/variable.js
@@ -0,0 +1,12 @@
+const accountId =144
+let accountemail = "hitesh@gmail.com"
+var accountpswd = "123"
+acountCity = "jaipur"
+
+// accountId=9
+accountemail="56789@gmail.com"
+accountpswd="d23"
+acountCity="jaipuria"
+console.log(accountId);
+
+console.table([accountId,accountemail,accountpswd,acountCity])
